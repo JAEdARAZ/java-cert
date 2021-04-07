cbb87a376293241ad9f522c82938b25654e51aba
Jaime Aranzazu <jaedaraz@gmail.com>
2021-04-07 20:06:04 +0200

    added instatiation block

--- staticMethodsAttrs/Counter.java
+++ staticMethodsAttrs/Counter.java
@@ -4,16 +4,22 @@ public class Counter {
     public static int instances = 0;
 
     public Counter(){
+        System.out.println("4. inside constructor");
         instances++;
     }
 
     //when the first instance is created, this will be executed first thing (even before constructor)
     static{
-        System.out.println("just once");
+        System.out.println("1. just once");
         printThis();
     }
 
+    //initialization block. Executed every instantiation
+    {
+        System.out.println("3. created instance");
+    }
+
     public static void printThis(){
-        System.out.println("printing from static method bro");
+        System.out.println("2. printing from static method bro");
     }
 }
--- staticMethodsAttrs/MainStatic.java
+++ staticMethodsAttrs/MainStatic.java
@@ -3,8 +3,8 @@ package staticMethodsAttrs;
 public class MainStatic {
     public static void main(String[] args) {
         new Counter();
-        System.out.println("instances: " + Counter.instances);
+        System.out.println("LAST: instances: " + Counter.instances);
         Counter c = new Counter();
-        System.out.println("instances: " + c.instances); //can be accessed with the instance too
+        System.out.println("LAST: instances: " + c.instances); //can be accessed with the instance too
     }
 }
